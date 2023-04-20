35676115d337cd3390c1ea6758675706994860c9
Kristin Laemmert <mildwonkey@users.noreply.github.com>
2023-04-20 10:00:58 -0400

    starting out

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+// what the fuck is a thema
+
+// what the fuck is a dashboard json
+
+// what the fuck is a dashboard schema
+
+// how the fuck do i mash those things together
+
+// now what
+
+// maybe try out constraint.cue, too
